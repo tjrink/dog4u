6412b87923886d4c98a0a4abce01e001f163433d
tjrink <tjrink@gmail.com>
2026-01-13 19:51:26 +1300

    Slider quiz now sends and receives values from database

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.get('/test', async (req, res) => {
   try {
     console.log("Attempting to connect to:", process.env.DATABASE_URL ? "URL found" : "URL NOT FOUND");const result = await pool.query('SELECT NOW()');
-    res.json({ message: "Server is running", db_time: result.rows[0].now });
+    res.json({ message: "Test test Server is running", db_time: result.rows[0].now });
   } catch (err) {
     console.error("Database error:", err.message); 
     res.status(500).send("Server Error");
@@ -42,6 +42,43 @@ app.post('/api/get_breed_nickname', async (req, res) => {
     res.status(500).json({error: "Could not fetch breed nickname"});
   }
 });
+
+//Get Breed scores based on allValues from sliders
+app.post('/api/get_slider_quiz_results', async (req, res) =>{
+  try {
+    //reception of slider values
+    const quizData = req.body;
+
+    //Breaks down the slider value object into component categories
+    const {
+      Playfulness,
+      Energy,
+      Affection,
+      ["Good With Strangers"]: goodWithStrangers, //Categories with spaces must be edited
+      ["Good With Children"]: goodWithChildren,
+      Drooling,
+      Shedding,
+      Trainability
+    } = req.body;
+
+    //Sends query to database
+    //Algorithm multiples the breed's score in a category by the user's priority level
+    //These results are summed to get the breed score
+    //Top 3 resulting breed scores are returned
+    //For "bad" categories, like drooling and shedding, the values are subtracted rather than added
+    const result = await pool.query('SELECT name, (affection_level * $1 + playfulness_level * $2 - drooling_level * $3 + energy_level * $4 + good_with_strangers_level * $5 + good_with_children_level * $6 - shedding_level * $7 + trainability_level * $8) AS breed_score FROM breeds ORDER BY breed_score DESC LIMIT 3', [Affection, Playfulness, Drooling, Energy, goodWithStrangers, goodWithChildren, Shedding, Trainability]);
+
+    //Sends results back to the React components
+    res.json({ 
+        message: "Data received successfully!", 
+        received: result.rows
+    });
+  } catch (err) {
+    console.error("Server error:", err.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 
 
 app.listen(PORT, () => {
